ac77fd0fa049d28baae595bd36711368e4bd3474
gabriellsilva12 <gsilva04950@gmail.com>
2026-02-18 20:32:04 -0300

    feat: initial structure of the açaí beats landing page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import HomePage from "./components/HomePage"
+import "./styles/global.css"
+import "./styles/themes.css"
+function App() {
+
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavMobile/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css"
+
+export default function NavMobile() {
+  return (
+    <div className={styles.header}>
+
+    </div>
+  )
+}
